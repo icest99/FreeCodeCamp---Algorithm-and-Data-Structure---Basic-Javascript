15e997e084e8d733c38d680f15d728c847e8c5fe
icest99 <playicest@gmail.com>
2021-02-02 18:07:34 +0700

    note-to-self. some of these are hard I will revisit it later.

--- Intermediate Algorithm Scripting/Sum All Primes.js
+++ Intermediate Algorithm Scripting/Sum All Primes.js
@@ -1,4 +1,45 @@
+function sumPrimes(num) {
+  //for generating a range of number
+  const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+  
+  //variable for accumulating the odd prime nubemrs
+  let sum = 0;
 
+  //function to check if it's a prime nubmer or
+  let isPrime = (num) => {
+    if (num <= 1) {
+    return true
+  } else if (num <= 3) {
+    return true
+  } else if (num%2 === 0 || num%3 === 0) {
+    return false
+  }
+ 
+  let i = 5
+  while (i*i <= num) {
+    if (num%i === 0 || num%(i+2) === 0) {
+      return false
+    }
+    i += 6
+  }
+  return true
+
+  };
+  
+  //generate a range of number
+  const numSeq = range(1,num,1);
+  
+  //check for odd numbers
+  numSeq.forEach(x => {
+  if (isPrime(x)){
+    if (x%2 != 0) {
+      sum += x;
+    }
+  }})
+  return sum + 1;
+}
+
+console.log(sumPrimes(10));
 
 
 
